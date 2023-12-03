ff9efad073a6d34e446142d890a31c710189b91f
chitalu7 <chitalu.am@gmail.com>
2023-12-03 08:23:39 -0800

    Started AoC Day 1

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ADVENT OF CODE - DAY 1 - TREBUCHET");
+
+Console.WriteLine("Press any key to exit...");
+
+Console.ReadKey();
